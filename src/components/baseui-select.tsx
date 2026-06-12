f88b229574d4ec541372756f48ed3c0c7faeee6d
PrateekP <prateekpanta1789@gmail.com>
2026-06-12 12:24:50 +0530

    Chore: added styles to match custom select

--- src/components/baseui-select.tsx
+++ src/components/baseui-select.tsx
@@ -11,8 +11,8 @@ const BaseUISelect = () => {
   return (
     <div>
       <Select.Root items={frameworks}>
-        <Select.Label className={"text-left text-black"}>Frontend Frameworks</Select.Label>
-        <Select.Trigger className={"w-50 bg-white text-neutral-600 text-left text-sm hover:text-neutral-900 hover:border-blue-300 p-2 rounded-md border border-neutral-200 cursor-pointer transition-all duration-200"}>
+        <Select.Label className={"text-left text-black dark:text-white"}>Frontend Frameworks</Select.Label>
+        <Select.Trigger className={"w-50 p-2 bg-white dark:bg-neutral-800 text-neutral-900 dark:text-neutral-50 text-left text-sm rounded-md border border-neutral-200 dark:border-neutral-700 cursor-pointer transition-all duration-200"}>
           <Select.Value placeholder="Select Framework" />
         </Select.Trigger>
         <Select.Portal>
@@ -20,16 +20,16 @@ const BaseUISelect = () => {
             alignItemWithTrigger={false}
           >
             <Select.Popup
-              className={"w-50 p-1 mt-1.5 bg-white border border-neutral-200 shadow-lg rounded-md"}
+              className={"w-50 p-1 mt-1.5 bg-white dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-700 shadow-lg rounded-md"}
             >
               {frameworks.map(opt => {
                 return (
                   <Select.Item
                     key={opt.value} 
                     value={opt.value}
-                    className={"p-2 rounded-md cursor-pointer select-none border border-transparent hover:bg-neutral-100 hover:border-neutral-200 data-highlighted:bg-neutral-100"}
+                    className={"p-2 hover:bg-neutral-100 dark:hover:bg-neutral-700 rounded-sm cursor-pointer select-none border border-transparent hover:border-neutral-200 dark:hover:border-neutral-600 data-highlighted:bg-neutral-100 data-highlighted:border-neutral-200 dark:data-highlighted:bg-neutral-700 dark:data-highlighted:border-neutral-600"}
                   >
-                    <Select.ItemText className={"text-left"}>{opt.label}</Select.ItemText>
+                    <Select.ItemText className={"text-left text-black dark:text-white text-sm"}>{opt.label}</Select.ItemText>
                   </Select.Item>
                 )
               })}
